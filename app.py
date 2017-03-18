8de5cd49452d958f58d02e3fc8d6005bff5d2316
Sean Nguyen <sean.nguyentrong@gmail.com>
2017-03-19 02:23:54 +0800

    Change prince to float

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ bit_coin_price = coin_base_client.get_spot_price(currency_pair = 'BTC-USD')
 
 elasticsearch = Elasticsearch()
 body = {
-  "timestamp": datetime.now(), 
-  "price": bit_coin_price.amount, 
+  "timestamp": datetime.now(),
+  "price": float(bit_coin_price.amount),
   "currency": bit_coin_price.currency
 }
+
 elasticsearch.index(index="coinbase-price", doc_type="test-type", body=body)
