d8879ef8074cdf24accfba1b75f016060ecc288d
Sean Nguyen <sean.nguyentrong@gmail.com>
2017-03-20 18:15:17 +0800

    Remove type mapping for ES index

--- lib/price_poller.py
+++ lib/price_poller.py
@@ -17,31 +17,12 @@ class BitCoinPriceTracker:
         return self.__coin_base_client.get_spot_price(urrency_pair = 'BTC-SGD')
 
     def __save_bit_coin_price(self, price, currency):
-        self.__create_es_index()
-
         body = {
             "price": float(price),
             "currency": currency,
             "timestamp": datetime.now()
         }
         self.__es_client.index(index=BitCoinPriceTracker.__ES_INDEX_NAME, doc_type="price_data", body=body)
-
-    def __create_es_index(self):
-        indices = self.__es_client.indices
-        if not indices.exists(index=BitCoinPriceTracker.__ES_INDEX_NAME):
-            index_config = {
-                "mappings": {
-                    "price_data": {
-                        "properties": {
-                            "price": {
-                                "type": "float",
-                            }
-                        }
-                    }
-                }
-            }
-            indices.create(index=BitCoinPriceTracker.__ES_INDEX_NAME, body=index_config)
-
 
 if __name__ == '__main__':
     BitCoinPriceTracker().snapshot_price()
