30fa580ee8c15ddf717738d4186612a41a0553ba
Sean Nguyen <sean.nguyentrong@gmail.com>
2017-05-03 18:30:24 +0800

    Add ethereum to price change tracker

--- lib/pollers/price_change.py
+++ lib/pollers/price_change.py
@@ -15,42 +15,52 @@ class PriceChangePoller(PricePoller):
         super(PriceChangePoller, self).__init__()
 
     def poll_price_change(self):
-        logging.info('Start polling price change')
-        past_price_data = self.__get_past_price()
+        self.__poll_and_alert_price_change('BTC')
+        self.__poll_and_alert_price_change('ETH')
+
+    def __poll_and_alert_price_change(self, type):
+        logging.info('Start polling %s price change' % type)
+        past_price_data = self.__get_past_price(type)
         if not past_price_data:
             return
 
-        logging.info('Price %s hours ago: %s %s' % (self.__PRICE_CHANGE_TIME_RANGE_HOURS,
-                                                   past_price_data['currency'],
-                                                   past_price_data['price']))
+        logging.info('%s price %s hours ago: %s %s' % (type,
+                                                       self.__PRICE_CHANGE_TIME_RANGE_HOURS,
+                                                       past_price_data['currency'],
+                                                       past_price_data['price']))
         current_price_data = self.__get_current_price()
-        logging.info('Price at the moment: %s %s' % (current_price_data['currency'], current_price_data['amount']))
+        logging.info('%s price at the moment: %s %s' % (type,
+                                                        current_price_data['currency'],
+                                                        current_price_data['amount']))
 
         change_ratio = float(current_price_data['amount']) / past_price_data['price']
-        logging.info('Price change ratio: %s' % change_ratio)
-        self.__save(change_ratio)
-        self.__alert(change_ratio)
+        logging.info('%s Price change ratio: %s' % (type, change_ratio))
+        self.__save(change_ratio, type)
+        self.__alert(change_ratio, type)
 
-    def __save(self, change_ratio):
-        logging.info('Saving Bit Coin change ratio to ElasticSearch')
+    def __save(self, change_ratio, type):
+        logging.info('Saving %s change ratio to ElasticSearch' % type)
         body = {
             "change_ratio": float(change_ratio),
-            "timestamp": datetime.utcnow()
+            "timestamp": datetime.utcnow(),
+            "type": type
         }
         self._es_client.index(index=self.__ES_INDEX_NAME, doc_type="price_change_data", body=body)
 
-    def __alert(self, change_ratio):
+    def __alert(self, change_ratio, type):
         lower_threshold = 1 - PriceChangePoller.__PRICE_CHANGE_THRESHOLD
         upper_threshold = 1 + PriceChangePoller.__PRICE_CHANGE_THRESHOLD
         if not(lower_threshold < change_ratio < upper_threshold):
-            payload = {"text": "Bit Coin price is changing quickly %s%% compare to %s hours ago"
-                               % (round(change_ratio * 100, 2), self.__PRICE_CHANGE_TIME_RANGE_HOURS)}
+            payload = {"text": "%s price is changing quickly %s%% compare to %s hours ago"
+                               % (type, round(change_ratio * 100, 2), self.__PRICE_CHANGE_TIME_RANGE_HOURS)}
             requests.post(configs.SLACK_WEB_HOOK, data={'payload': str(payload)})
-            
 
-    def __get_past_price(self):
+    def __get_past_price(self, type):
         query_body = {
             'query': {
+                'match': {
+                    "type": type
+                },
                 'range': {
                     'timestamp': {
                         'gte': (datetime.utcnow() - timedelta(hours=self.__PRICE_CHANGE_TIME_RANGE_HOURS)).isoformat(),
