f5bb237df45e3a671b5c6db9ea7dc317c1c21ff6
Sean Nguyen <sean.nguyentrong@gmail.com>
2018-04-08 13:03:38 +0800

    Add db migration for snapshots

---
+++ db/migrations/20180408034411_CreateSnapshotsTable.sql
@@ -0,0 +1,21 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+CREATE SEQUENCE snapshots_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE snapshots (
+  id INT NOT NULL PRIMARY KEY DEFAULT nextval('snapshots_id_seq'),
+  created_at TIMESTAMP
+);
+
+CREATE INDEX snapshots_on_created_at ON snapshots USING btree(created_at);
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+DROP TABLE snapshots;
+DROP SEQUENCE snapshots_id_seq;
